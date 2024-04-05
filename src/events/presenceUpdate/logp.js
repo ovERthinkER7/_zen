8b9873a20e1fdb5523920b3766b282e4392ad33d
ovERthinkER7 <darshandd089@gamail.com>
2024-04-05 23:18:16 +0530

    fixed bug in presence logger system

--- src/events/presenceUpdate/logp.js
+++ src/events/presenceUpdate/logp.js
@@ -2,24 +2,27 @@ const { EmbedBuilder } = require("discord.js");
 const plog = require("../../models//presencelog");
 
 module.exports = async (oldp, newp, client, handler) => {
-    var data = await plog.findOne({ Guild: oldp?.guild.id || "0" });
-    // console.log(data + "fjdlk");
-    if (!data) return;
-    if (oldp.user.bot) return;
-    if (oldp.status == newp.status) return;
+    if (!newp.guild || newp.user.bot) return;
+
     // console.log(oldp.user.displayAvatarURL({ dynamic: true }));
-    var sendchannel = await oldp.guild.channels.fetch(data.Logchannel);
+    var data = await plog.findOne({ Guild: newp.guild.id });
+    if (!data) return;
+    var sendchannel = await newp.guild.channels.fetch(data.Logchannel);
     var embed;
+    var old;
+    if (!oldp) old = "offline";
+    else old = oldp.status;
+    if (old == newp.status) return;
     try {
         embed = new EmbedBuilder()
             .setColor("Aqua")
             .setDescription(
-                `${oldp.user} presence's changes from **${oldp.status}** to **${newp.status}**`
+                `${newp.user} presence's changes from **${old}** to **${newp.status}**`
             )
             .setTimestamp()
             .setAuthor({
-                name: oldp.user.username,
-                iconURL: oldp.user.displayAvatarURL({ dynamic: true }),
+                name: newp.user.username,
+                iconURL: newp.user.displayAvatarURL({ dynamic: true }),
             });
     } catch (err) {
         console.log(err);
