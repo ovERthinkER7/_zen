43d1752142ef77679da5404fc0df08d8fcefb83d
ovERthinkER7 <darshandd089@gamail.com>
2024-04-11 21:00:21 +0530

    Change defalut port number for express to process.

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const server = express();
+const portNo = process.env.PORT || 8000;
 
 server.all(`/`, (req, res) => {
     res.send(`Please connect me to a hosting website in-order to work 24/7.`);
 });
 
 function keepAlive() {
-    server.listen(3000, () => {
+    server.listen(portNo, () => {
         console.log(`on`);
     });
 }
