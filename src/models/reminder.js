6cece0532c30171375a7d30aaf2cadb1f2713fce
ovERthinkER7 <darshandd089@gamail.com>
2024-04-15 20:46:08 +0530

    added uniqueID in reminderschema

--- src/models/reminder.js
+++ src/models/reminder.js
@@ -1,6 +1,14 @@
+const { nanoid } = require("nanoid");
 const { model, Schema } = require("mongoose");
 
 let reminderschema = new Schema({
+    uniqueId: {
+        type: String,
+        required: true,
+        default: () => nanoid(7),
+        index: { unique: true },
+    },
+    Guild: String,
     User: String,
     Time: String,
     Remind: String,
